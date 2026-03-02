959c5900976c49d75e7be06ffbc43e36c729e89d
Marco Dominguez <mardom640@gmail.com>
2026-03-02 07:21:40 -0700

    Initial Commit

---
+++ PROYECTO_4_NAVIGATION/PROYECTO_4_NAVIGATIONApp.swift
@@ -0,0 +1,17 @@
+//
+//  PROYECTO_4_NAVIGATIONApp.swift
+//  PROYECTO_4_NAVIGATION
+//
+//  Created by Marco Dominguez on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PROYECTO_4_NAVIGATIONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
